37329777bc4371c37f80fbc0c40a00a2f3d41b38
Francisco Domínguez <d3vcho@protonmail.com>
2020-03-08 16:26:15 +0100

    S3 aún sin funcionar

--- P1/S3/src/s3/Simulador.java
+++ P1/S3/src/s3/Simulador.java
@@ -4,11 +4,16 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Simulador extends Observable implements Runnable {
     
     private float temperatura;
     volatile boolean ejecutar = true;
+    private GUIBoton boton = new GUIBoton();
+    private GUIGrafica grafica = new GUIGrafica();
+    private GUIPantalla pantalla = new GUIPantalla();
     
     public float getTemperatura() {
         return this.temperatura;
@@ -21,16 +26,14 @@ public class Simulador extends Observable implements Runnable {
     }
     
     @Override
-    public void run() {
+    public void run(){
         while(this.ejecutar) {
+            this.pantalla.setObservable(this);
             // Mínimo y máximo de temperatura. Generamos una entre medias.  
             float max = 45, min = -10;
             Random rand = new Random();
             float temp = min + rand.nextFloat() * (max - min);
-
-            System.out.println("Temperatura cambiada: " + temp);
-
-            this.temperatura = temp;
+            this.setTemperatura(temp);
 
             try {
                 //Ponemos a "Dormir" el programa durante los ms que queremos
@@ -41,6 +44,10 @@ public class Simulador extends Observable implements Runnable {
         }
     }
     
+    public boolean getEstado() {
+        return this.ejecutar;
+    }
+    
     public void detener() {
         this.ejecutar = false;
     }
@@ -48,62 +55,19 @@ public class Simulador extends Observable implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         // Creamos el observable
         Simulador sim = new Simulador();
+        sim.pantalla.setObservable(sim);
         
-        // Creamos el observador graficaTemperatura
-        graficaTemperatura grafica = new graficaTemperatura();
+        sim.addObserver(sim.boton);
+        sim.addObserver(sim.grafica);
         
-        // Lo añadimos a la lista
-        sim.addObserver(grafica);
-        
-        // Creamos el observador botonCambio
-        botonCambio boton = new botonCambio();
-        
-        // Lo añadimos a la lista
-        sim.addObserver(boton);
-        
-        // Creamos el observador pantallaTemperatura
-        pantallaTemperatura pantalla = new pantallaTemperatura();
-        
-        // Pedimos el número de simulaciones
-        int i = 0;
-        try {
-            System.out.println("Introduca el número de simulaciones");
-            Scanner in = new Scanner(System.in);
-            i = in.nextInt();
-        } catch(Exception e) {
-            System.out.println(e);
-        }
         
         // Iniciamos la hebra
         Thread h1 = new Thread(sim);
         h1.start();
         
-        int contador = 0;
-        /* Dormimos el programa para notificar a los observadores para evitar que se generen más temperaturas de las que podemos comunicar */
-        while(contador != i){
-            TimeUnit.SECONDS.sleep(1);
-            System.out.println("Notificamos a los observadores...");
-            sim.setTemperatura(sim.getTemperatura());
-            
-            // Generamos aleatoriedad para saber si PantallaTemperatura quiere obtener la temperatura
-            int randomNum = ThreadLocalRandom.current().nextInt(0, 1 + 1);
-            
-            if(randomNum == 1) {
-                System.out.println("Observador Pantalla pide la temperatura");
-                pantalla.pedirTemperatura(sim);
-            }
-            
-            System.out.println("\tTemperatura Gráfica: " + grafica.getTemperatura());
-            System.out.println("\tTemperatura Botón: " + boton.getTemperatura());
-            System.out.println("\tTemperatura Pantalla: " + pantalla.getTemperatura());
-            
-            TimeUnit.SECONDS.sleep(5);
-            contador++;
-            
-            /* Después de un determinado número de ejecuciones, paramos el programa */
-            if(contador == i){
-                sim.detener();
-            }
-        }
+        sim.boton.main(new String[0]);
+        sim.grafica.main(new String[0]);
+        sim.pantalla.main(new String[0]);
+
     }
 }
